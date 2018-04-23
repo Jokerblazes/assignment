346d1773a954cd654555c61fb3028ab1dd105e0e
Jokerblazes <zcd123@qq.com>
2018-04-23 19:37:26 +0800

    complete the Prime Factors Exercise

--- src/main/java/com/joker/PrimeFactorsExercise.java
+++ src/main/java/com/joker/PrimeFactorsExercise.java
@@ -1,9 +1,28 @@
 package com.joker;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @Author Joker
  * @Description
  * @Date Create in 下午3:46 2018/4/23
  */
 public class PrimeFactorsExercise {
+    public List<Integer> generate(int number) {
+        if (number == 1)
+            return new ArrayList<>();
+        List<Integer> result = new ArrayList<>();
+        int i = 2;
+        while (number >= i) {
+            if (number % i == 0) {
+                number /= i;
+                result.add(i);
+                i = 2;
+            } else {
+                i++;
+            }
+        }
+        return result;
+    }
 }
--- src/test/java/com/joker/PrimeFactorsExerciseTest.java
+++ src/test/java/com/joker/PrimeFactorsExerciseTest.java
@@ -1,5 +1,11 @@
 package com.joker;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
 import static org.junit.Assert.*;
 
 /**
@@ -8,5 +14,20 @@ import static org.junit.Assert.*;
  * @Date Create in 下午3:48 2018/4/23
  */
 public class PrimeFactorsExerciseTest {
+    private PrimeFactorsExercise primeFactorsExercise;
+
+    @Before
+    public void setup() {
+        primeFactorsExercise = new PrimeFactorsExercise();
+    }
+
+    @Test
+    public void generateTest() {
+        List<Integer> result2 = Arrays.asList(2, 3, 5);
+        List<Integer> list1 = primeFactorsExercise.generate(1);
+        List<Integer> list2 = primeFactorsExercise.generate(30);
+        assertEquals(list1.size(), 0);
+        assertEquals(list2,result2);
+    }
 
 }

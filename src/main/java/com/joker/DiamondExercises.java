27014e3473531d16793db53a5df728f933cbbb48
Jokerblazes <zcd123@qq.com>
2018-04-23 18:57:26 +0800

    Reconstruction

--- src/main/java/com/joker/DiamondExercises.java
+++ src/main/java/com/joker/DiamondExercises.java
@@ -10,6 +10,31 @@ public class DiamondExercises {
     public void isoscelesTriangle(int n) {
         int space = n - 1;
         int num = 1;
+        getTopIsoscelesTriangle(n, space, num);
+    }
+
+    public void diamond(int n) {
+        int space = n - 1;
+        int num = 1;
+        getTopIsoscelesTriangle(n,space,num);
+        space = 1;
+        num = (n - 1) * 2 - 1;
+        getBottomIsoscelesTriangle(n, space, num);
+    }
+
+
+    public void diamondWithName(int n, String bill) {
+        int space = n - 1;
+        int num = 1;
+        getTopIsoscelesTriangle(n - 1,space,num);
+        System.out.println(bill);
+        space = 1;
+        num = (n - 1) * 2 - 1;
+        getBottomIsoscelesTriangle(n, space, num);
+    }
+
+
+    private void getTopIsoscelesTriangle(int n, int space, int num) {
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < space; j++) {
                 System.out.print(" ");
@@ -24,23 +49,7 @@ public class DiamondExercises {
         }
     }
 
-    public void diamond(int n) {
-        int space = n - 1;
-        int num = 1;
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < space; j++) {
-                System.out.print(" ");
-            }
-            for (int j = 0; j < num; j++) {
-                System.out.print("*");
-            }
-
-            space--;
-            num += 2;
-            System.out.println();
-        }
-        space = 1;
-        num -= 4;
+    private void getBottomIsoscelesTriangle(int n, int space, int num) {
         for (int i = n - 1; i >= 1; i--) {
             for (int j = 0; j < space; j++) {
                 System.out.print(" ");
@@ -52,38 +61,5 @@ public class DiamondExercises {
             num -= 2;
             System.out.println();
         }
-    }
-
-
-    public void diamondWithName(int n, String bill) {
-        int space = bill.length() / 2;
-        int num = 1;
-        for (int i = 0; i < n - 1; i++) {
-            for (int j = 0; j < space; j++) {
-                System.out.print(" ");
-            }
-            for (int j = 0; j < num; j++) {
-                System.out.print("*");
-            }
-
-            space--;
-            num += 2;
-            System.out.println();
-        }
-        System.out.println(bill);
-        space++;
-        num -= 2;
-        for (int i = n - 1; i >= 1; i--) {
-            for (int j = 0; j < space; j++) {
-                System.out.print(" ");
-            }
-            for (int j = 0; j < num; j++) {
-                System.out.print("*");
-            }
-            space++;
-            num -= 2;
-            System.out.println();
-        }
-
     }
 }

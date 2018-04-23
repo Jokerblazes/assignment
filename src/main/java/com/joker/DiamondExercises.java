bdad6579eb3b10992ba590c296c9ea83f25c5de4
Jokerblazes <zcd123@qq.com>
2018-04-23 16:22:24 +0800

    complete the fifth task(Isosceles Triangle)

--- src/main/java/com/joker/DiamondExercises.java
+++ src/main/java/com/joker/DiamondExercises.java
@@ -7,4 +7,20 @@ package com.joker;
  */
 public class DiamondExercises {
 
+    public void isoscelesTriangle(int n) {
+        int space = n - 1;
+        int num = 1;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < space; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < num; j++) {
+                System.out.print("*");
+            }
+
+            space--;
+            num+= 2;
+            System.out.println();
+        }
+    }
 }
--- src/test/java/com/joker/DiamondExercisesTest.java
+++ src/test/java/com/joker/DiamondExercisesTest.java
@@ -1,5 +1,11 @@
 package com.joker;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
 import static org.junit.Assert.*;
 
 /**
@@ -8,5 +14,23 @@ import static org.junit.Assert.*;
  * @Date Create in 下午3:47 2018/4/23
  */
 public class DiamondExercisesTest {
+    private DiamondExercises diamondExercises;
+    private ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    @Before
+    public void setup() {
+        System.setOut(new PrintStream(outContent));
+        diamondExercises = new DiamondExercises();
+    }
+
+    @Test
+    public void isosceles_triangle_test() {
+        String result = "  *\n ***\n*****\n";
+        diamondExercises.isoscelesTriangle(3);
+        assertEquals(systemOut(),result);
+    }
+
+    private String systemOut() {
+        return outContent.toString();
+    }
 
 }
